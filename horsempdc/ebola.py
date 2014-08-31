e477367ab2b22d3379dbe912d95d4fe029902ea7
Jurriaan Bremer <jurriaanbremer@gmail.com>
2014-08-31 13:23:21 +0200

    basis for handling key events, status line, some comments

--- horsempdc/ebola.py
+++ horsempdc/ebola.py
@@ -19,13 +19,7 @@ class Curse(object):
         'bands',
     ]
 
-    CHARACTERS = {}
     LOCALE = locale.getpreferredencoding()
-
-    for key in dir(curses):
-        value = getattr(curses, key)
-        if isinstance(value, (int, long)):
-            CHARACTERS[value] = key
 
     def __init__(self):
         self.stdscr = curses.initscr()
@@ -34,6 +28,8 @@ class Curse(object):
         self.stdscr.keypad(1)
         curses.curs_set(0)
 
+        self.height, self.width = self.stdscr.getmaxyx()
+
     def finish(self):
         curses.nocbreak()
         self.stdscr.keypad(0)
@@ -41,23 +37,31 @@ class Curse(object):
         curses.endwin()
 
     def draw_menu(self, borders=True):
-        self.height, width = self.stdscr.getmaxyx()
+        self.column_width = self.width / len(self.MENU)
 
-        self.column_width = width / len(self.MENU)
-
+        # Horizontal lines. One on top to distinguish the menu, one on the
+        # bottom to distinguish the status line.
         if borders:
-            self.stdscr.hline(1, 0, curses.ACS_HLINE, width)
+            self.stdscr.hline(1, 0, curses.ACS_HLINE, self.width)
+            self.stdscr.hline(self.height - 2, 0, curses.ACS_HLINE, self.width)
 
+        # Draw each menu.
         for idx, row in enumerate(self.MENU):
             self.stdscr.addstr(0, self.column_width * idx,
                                '%d: %s' % (idx + 1, row))
 
+            # Vertical line to distinguish between the various menus.
             if idx and borders:
                 self.stdscr.vline(0, self.column_width * idx - 1,
-                                  curses.ACS_VLINE, self.height)
+                                  curses.ACS_VLINE, self.height - 2)
 
+        # Plus sign at points where horizontal and vertical lines cross in
+        # the menu and "bottom tee" signs where the vertical lines and the
+        # status line meet.
         for idx in xrange(1, len(self.MENU)):
             self.stdscr.addch(1, self.column_width * idx - 1, curses.ACS_PLUS)
+            self.stdscr.addch(self.height - 2, self.column_width * idx - 1,
+                              curses.ACS_BTEE)
 
     def _draw_pad(self, index, lines):
         pad = curses.newpad(len(lines), self.column_width - 1)
@@ -67,7 +71,7 @@ class Curse(object):
 
         pad.refresh(0, 0,
                     2, self.column_width * index,
-                    self.height - 1, self.column_width * (index + 1) - 1)
+                    self.height - 3, self.column_width * (index + 1) - 1)
 
         return pad
 
@@ -78,8 +82,23 @@ class Curse(object):
             2: self._draw_pad(2, bands),
         }
 
+    def status(self, line, *args):
+        # Apply any arguments if given.
+        line = line % args if args else line
+
+        # Pad the line until the end of the screen.
+        line += ' ' * (self.width - len(line) - 1)
+
+        self.stdscr.addstr(self.height - 1, 0, line)
+
     def wait(self):
         self.stdscr.refresh()
-        ch = self.stdscr.getch()
-        log.debug('Received character %d (%s)',
-                  ch, self.CHARACTERS.get(ch, ch))
+        ch = self.stdscr.getkey()
+        log.debug('Received character %r', ch)
+
+        if not hasattr(self, '_handle_%s' % ch):
+            self.status('Unknown keybinding: %s', ch)
+        else:
+            getattr(self, '_handle_%s' % ch)()
+
+        self.stdscr.refresh()
